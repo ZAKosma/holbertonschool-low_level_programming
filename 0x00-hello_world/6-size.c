c464db0986405bff6559daaa3e858aa59ccf5c97
ZAKosma <zakosma@gmail.com>
2022-08-03 12:49:43 -0700

    C 5

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %u byte(s)\n", (int)sizeof(char));
+	printf("Size of an int: %u byte(s)\n", (int)sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", (int)sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", (int)sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", (int)sizeof(float));
+	
+	return (0);
+}
