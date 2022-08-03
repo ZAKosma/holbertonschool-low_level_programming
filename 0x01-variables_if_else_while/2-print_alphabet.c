c5c2ac79404bbfe68f52214a07895a2fdc4af74d
ZAKosma <zakosma@gmail.com>
2022-08-03 13:34:14 -0700

    C1 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 26; i++)
+	{
+		putchar(i + 97);
+	}
+	putchar(10);
+	return (0);
+}
