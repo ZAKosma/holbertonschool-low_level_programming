e8f39cd8c20e393f006b0b39440dfceff8d8bd44
ZAKosma <zakosma@gmail.com>
2022-08-04 12:32:07 -0700

    C2 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,12 +12,18 @@ void times_table(void)
 {
 	int x;
 	int y;
+	int outNum;
 
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
-			_putchar((x * y) + '0');
+			outNum = x * y;
+			if (outNum > 9)
+			{
+				_putchar(((outNum /10) % 10) + '0');
+			}
+			_putchar((outNum % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
