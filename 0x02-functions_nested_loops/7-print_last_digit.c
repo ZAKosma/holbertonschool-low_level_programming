7ee6c175a54357256c58384d0d6ea490daacd447
ZAKosma <zakosma@gmail.com>
2022-08-04 12:00:44 -0700

    C2 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* print_last_digit -prints _putchar using the defined _putchar from main.h
+*
+* @n: is a number that is passed
+*
+* Return: 0
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
