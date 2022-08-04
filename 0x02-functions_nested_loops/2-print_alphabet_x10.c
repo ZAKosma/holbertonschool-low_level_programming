06a754f0d988e23f9d90926a98bf2307a72f4254
ZAKosma <zakosma@gmail.com>
2022-08-04 11:12:12 -0700

    C2 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-* print_alphabet -prints _putchar using the defined _putchar from main.h
+* print_alphabet_x10 -prints _putchar using the defined _putchar from main.h
 *
 * Return: 0
 */
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	int i;
 	int count;
 
-	for (count = 0; i < 10; count++)
+	for (count = 0; count < 10; count++)
 	{
 		for (i = 0; i < 26; i++)
 		{
