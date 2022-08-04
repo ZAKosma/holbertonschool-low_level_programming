c46aad669e5b309a7e583ed637c1432171c17f39
ZAKosma <zakosma@gmail.com>
2022-08-04 08:56:35 -0700

    C2 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
 * main -prints _putchar using the defined _putchar from main.h
 *
 * Return: 0
 */
-int main(void)
+int print_alphabet(void)
 {
 	int i;
 
