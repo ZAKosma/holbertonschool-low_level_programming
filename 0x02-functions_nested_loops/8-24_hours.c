0a0d359844f5b186631d41665da6f6febe8bd03d
ZAKosma <zakosma@gmail.com>
2022-08-04 12:20:06 -0700

    C2 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* jack_bauer -prints _putchar using the defined _putchar from main.h
+*
+*
+* Return: 0
+*/
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(((h / 10) % 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(((m / 10) % 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar(10);
+		}
+	}
+}
