c95ed2e0e5d6182ac75a3dee72b0de6eab64afaf
ZAKosma <zakosma@gmail.com>
2022-08-04 12:39:34 -0700

    C2 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* add -prints _putchar using the defined _putchar from main.h
+*
+* @x: this is the first number
+* @y: this is the second number
+*
+* Return: 0
+*/
+int add(int x, int y)
+{
+	int x;
+	int y;
+
+	return (x * y);
+}
