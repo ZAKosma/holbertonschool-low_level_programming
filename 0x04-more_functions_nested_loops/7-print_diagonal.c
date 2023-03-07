b9cb3c51a81cee0c41fe5102bbf14492dbb4e083
ZAKosma <zakosma@gmail.com>
2023-03-07 08:58:25 -0800

    yes

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - returns the upperase character
+ * Return: uppercase character
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+       		_putchar (95);
+	}
+       	_putchar (10);
+}
